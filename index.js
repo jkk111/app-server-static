a16a669c5196587a07a5fe55fde6e20346a622a7
jkk111 <kellyj58@tcd.ie>
2018-03-27 09:47:44 +0100

    Removed Logging Socket

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ let socket = process.env.socket || 8080;
 let express = require('express');
 let app = express();
 
-const STATIC_PATH = `${__dirname}/static`
+const STATIC_PATH = process.env.STATIC_CONTENT;
 app.use(express.static(STATIC_PATH))
-
-console.log(socket);
-
 app.listen(socket)
